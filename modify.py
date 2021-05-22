3fcdb770e158d90227d51cb3c937379ec05f4c30
H4cKr0pZ <83129610+H4cKr0pZ@users.noreply.github.com>
2021-05-22 21:12:33 +0530

    Add files via upload

---
+++ modify.py
@@ -0,0 +1,89 @@
+#!/usr/python/H4CKR0PZ
+#for educational purposes..only!!!!!!!!!!!!
+ 
+import os
+import sys
+import time
+from time import sleep
+from os import system
+
+a=('clear')
+os.system(a)
+print("""
+
+               ===========THIS===TOOL==IS===MADE==BY===H4CKR0PZ==============
+              ||   ------>follow me on insta @_rough_life_rider_<--------   ||
+              ||                                                            ||
+              ||      this tool will modify your links....for attack        ||
+              ||    select the social media,which you use for attack..!     ||
+              ||                                                            ||
+              ||                                                            ||
+	      ||                                                            ||
+              _______________________________________________________________
+              ===============================================================
+
+ """)
+
+
+
+
+sleep(1)
+print("""       =======================================
+                +++++++++++++++++++++++++++++++++++++++
+                1-INSTAGRAM
+                2-FACEBOOK
+                3-WHATSAPP
+                4-TWITTER
+                5-LINKEDIN
+                6-GOOGLE
+                =======================================""")
+
+B=int(input("choose one option from 1-5: "))
+
+D=input("paste the phishing link here without the (https://): ")
+
+print("""
+
+
+
+ """)
+
+
+
+
+
+
+
+
+while True:
+    if B==1:
+        print(" send it to the victim: link==> https://instagram.com-@".strip(),D, sep="")
+        break
+    elif B==2:
+        print(" send it to the victim: link==> https://facebook.com-@".strip(),D, sep="")
+        break
+    elif B==3:
+        print(" send it to the victim: link==> https://web.whatsapp.com-@".strip(),D, sep="")
+        break
+    elif B==4:
+        print(" send it to the victim: link==> https://twitter.com-@".strip(),D, sep="")
+        break
+    elif B==5:
+        print(" send it to the victim: link==> https://in.linkedin.com-@".strip(),D, sep="")
+        break
+    elif B==6:
+        print(" send it to the victim: link==> https://accounts.google.com-@" .strip(),D, sep="")
+        break
+
+
+
+
+
+
+
+
+print("""
+
+
+         ========================use=this=for=educational=purpose=================""")
+print(" =======================H4CKROPZ..==>https://github.com/H4ckr0pZ============")
